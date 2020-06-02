ee4d0ef8ff70b83bc8323470fa5db00f4bd32ef8
bingkongmaster <bingkongmaster@gmail.com>
2020-06-02 09:46:03 +0900

    + Added addslashes() vs command and sql attacks

--- root/add_joke.php
+++ root/add_joke.php
@@ -4,14 +4,16 @@ include "db_connect.php";
 $new_joke_question = $_GET["newjoke"];
 $new_joke_answer = $_GET["newanswer"];
 
+$new_joke_question = addslashes($new_joke_question);
+$new_joke_answer = addslashes($new_joke_answer);
 //Search database for key word
 echo "<h2>Added: $new_joke_question </h2>";
 echo "<h2>Added: $new_joke_answer </h2>";
 $sql = "INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer) VALUES (null, '$new_joke_question', '$new_joke_answer')";
-$result = $mysqli->query($sql);
+$result = $mysqli->query($sql) or die(mysqli_error($mysqli));
 
 include "search_all_jokes.php";
 
 ?>
 
-<a href="index.php"Return</a>
+<a href="index.php">Return</a>
